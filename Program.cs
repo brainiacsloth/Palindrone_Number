15d321c4045d7519d5c352ba4c0d6b447010edd9
User <User@LAPTOP-1ND091FI>
2024-01-06 13:25:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrone_Number
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+
+            if (IsPalindrome(input))
+            {
+                Console.WriteLine($"{input} is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{input} is not a palindrome.");
+            }
+        }
+
+        static bool IsPalindrome(string input)
+        {
+            int left = 0;
+            int right = input.Length - 1;
+
+            while (left < right)
+            {
+                if (input[left] != input[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+    }
+}
